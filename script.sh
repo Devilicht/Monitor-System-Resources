1ffb0ca17de7363d1d88c3dae439979567969f5b
Devilicht <oblivionccyber@gmail.com>
2023-05-14 13:02:19 -0400

    fix echo

--- script.sh
+++ script.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-MEM_LIMIT=30
-CPU_LIMIT=30
-DISK_LIMIT=30
+MEM_LIMIT=70
+CPU_LIMIT=70
+DISK_LIMIT=70
 
-MEM_USAGE=$(free | awk 'FNR == 2 {print $3/$2 * 100}')
-CPU_USAGE=$(top -bn1 | grep 'Cpu(s)' |  sed 's/.*, *\([0-9]*\)%* id.*/\1/')
-DISK_USAGE=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
+while true
+do
+    MEM_USAGE=$(free | awk 'FNR == 2 {print $3/$2 * 100}')
+    CPU_USAGE=$(top -bn1 | grep 'Cpu(s)' | sed 's/.*, *\([0-9.]*\)%* id.*/\1/')
+    DISK_USAGE=$(df -h / | awk '{print $5}' | tail -1 | sed 's/%//')
 
-if (($( echo "$MEM_USAGE" > "$MEM_LIMIT" | bc -l ) )); then
-    echo "ALERTA: the memory is being used above the limit $MEM_LIMIT%"
-fi
+    echo "$(date +%T) - Uso de memória: $MEM_USAGE%"
+    echo "$(date +%T) - Uso de CPU: $CPU_USAGE%"
+    echo "$(date +%T) - Uso de disco: $DISK_USAGE%"
 
-if (($( echo "$CPU_USAGE" > "$CPU_LIMIT" | bc -l ) )); then
-    echo "ALERTA: the CPU is being used above the limit $CPU_LIMIT%"
-fi
+    if (( $(echo "$MEM_USAGE > $MEM_LIMIT" | bc -l) )); then
+        echo "$(date +%T) ALERTA: Uso de memória acima de $MEM_LIMIT% ($MEM_USAGE%)"
+    fi
 
-if (($( echo "$DISK_USAGE" > "$DISK_LIMIT" | bc -l ) )); then
-    echo "ALERTA: the disk is being used above the limit $DISK_LIMIT%"
-fi
+    if (( $(echo "$CPU_USAGE > $CPU_LIMIT" | bc -l) )); then
+        echo "$(date +%T) ALERTA: Uso de CPU acima de $CPU_LIMIT% ($CPU_USAGE%)"
+    fi
+
+    if (( $(echo "$DISK_USAGE > $DISK_LIMIT" | bc -l) )); then
+        echo "$(date +%T) ALERTA: Uso de disco acima de $DISK_LIMIT% ($DISK_USAGE%)"
+    fi
+
+    echo " "    
+    sleep 10
+done
